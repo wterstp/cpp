067e177726b3033dd2d1c739e7ff9b72668b6ff6
winter <1294248854@qq.com>
2024-03-28 19:46:16 +0800

    Adding files

---
+++ 19_Word_Count_unordered_map/dictionary.cc
@@ -0,0 +1,64 @@
+#include <cctype> // For std::isalpha
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Dictionary {
+public:
+  void read(const string filename);
+  void store(const string filename);
+
+private:
+  unordered_map<string, int> _dict;
+};
+
+void Dictionary::read(const string filename) {
+  ifstream file(filename);
+  if (!file.is_open()) {
+    cerr << "Error: Could not open " << filename << endl;
+    return;
+  }
+  string word;
+  while (file >> word) {
+    string clean_word;
+    for (char c : word) {
+      if (isalpha(c)) {
+        clean_word += tolower(c);
+      }
+    }
+    if (!clean_word.empty()) {
+      ++_dict[clean_word];
+    }
+  }
+  file.close();
+}
+
+void Dictionary::store(const string filename) {
+  ofstream file(filename);
+  if (!file.is_open()) {
+    cerr << "Error: Could not open " << filename << endl;
+    return;
+  }
+  for (auto &pair : _dict) {
+    file << pair.first << " " << pair.second << "\n";
+  }
+  file.close();
+}
+
+void test0() {
+  Dictionary dict;
+  string input_file = "The_Holy_Bible.txt";
+  string output_file = "dict.txt";
+  dict.read(input_file);
+  dict.store(output_file);
+};
+
+int main() {
+  time_t start = time(NULL);
+  test0();
+  time_t end = time(NULL) - start;
+  cout << end << "s" << endl;
+
+  return 0;
+}
